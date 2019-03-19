3e1ade91a62e7f5ee2651dcb179e4b925b448f82
Ancel Miller <ancelm@OMA-MAC-00132.local>
2019-03-18 22:25:01 -0700

    Login Register

---
+++ routes/views/purchase.js
@@ -0,0 +1,28 @@
+var keystone = require('keystone');
+
+exports = module.exports = function (req, res) {
+	var Product = keystone.list('Product');
+	var view = new keystone.View(req, res);
+	var locals = res.locals;	
+
+	// locals.section is used to set the currently selected
+	// item in the header navigation.
+	locals.title = 'WSC | ' + req.params.slug;
+	locals.section = 'Purchase';
+	console.log(req.params.slug);
+	
+	// Load featured products
+	view.on('init', next => {
+		Product.model.findOne({
+				slug: req.params.slug,
+				publish: true
+			})
+			.exec(function (err, results) {
+				locals.product = results;		
+				next(err);
+			});
+  });
+  
+	// Render the view
+	view.render('purchase');
+};
